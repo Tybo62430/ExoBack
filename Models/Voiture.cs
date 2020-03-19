75095dae4d9fad07c9660882be7b730afb7e0602
Thibault <derbomez.thibault@gmail.com>
2020-03-19 15:28:20 +0100

    Models

---
+++ Models/Voiture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExoBack.Models
+{
+    public class Voiture
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Marque { get; set; }
+        public string Imatriculation { get; set; }
+        public Utilisateur Proprietaire { get; set; }
+    }
+}
